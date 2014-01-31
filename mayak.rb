0b9e3329e01d9e963d18c8b6f0a5908ca71a5a01
Matt Royer <matthew.royer@gmail.com>
2014-01-30 17:08:57 -0700

    First commit for Mayak!

---
+++ mayak.rb
@@ -0,0 +1,36 @@
+require 'fileutils'
+
+class Mayak
+
+  def initialize(base_directory, directories_and_files)
+    @base_directory = base_directory
+    @directories_and_files = directories_and_files
+    @current_dir = Dir.entries("#{@base_directory}").select { |file| File.file?("#{@base_directory}/#{file}") }
+
+    move_files
+  end
+
+  def move_files
+    create = Proc.new do |dir|
+      FileUtils.mkdir_p(dir)
+    end
+
+    @directories_and_files.each do |dir, types|
+      files = @current_dir.select { |f| f.match /#{types.join('|')}/i }
+      create.call "#{@base_directory}/#{dir}"
+
+      move = Proc.new do |arr|
+        arr.each do |element|
+          FileUtils.mv("#{@base_directory}/#{element}", "#{@base_directory}/#{dir}/#{element}")
+        end
+      end
+
+      move.call files
+    end
+  end
+end
+
+base = "#{ENV['USERPROFILE']}/Desktop"
+directories_and_files = {:Images => ['jpg', 'jpeg', 'png', 'gif'], :PDFs => ['pdf'], :Spreadsheets => ['xls', 'xlsx'], :Sites => ['url', 'html', 'htm'], :Docs => ['txt', 'doc', 'docx', 'rtf']}
+
+Mayak.new base, directories_and_files
